7006e5df5ef69333f88c2275d146423ea9fb409e
shabnamarasteh <64069037+shabnamarasteh@users.noreply.github.com>
2021-05-02 11:38:52 -0800

    create project

---
+++ src/test/java/com/test/functional/FunctionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.functional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
