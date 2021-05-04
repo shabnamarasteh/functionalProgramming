aa36aef2fc766c1af7621537c56b6ca6a88fc3b9
shabnamarasteh <64069037+shabnamarasteh@users.noreply.github.com>
2021-05-04 11:11:40 -0800

    add RunnableClass for test functional interface

---
+++ src/main/java/com/test/functional/_Runnable.java
@@ -0,0 +1,21 @@
+package com.test.functional;
+
+public class _Runnable {
+    public static void main(String[] args) {
+        //Runnable Interface With lambda Expression
+        Runnable helloWorld = () -> System.out.println("HelloWorld");
+        helloWorld.run();
+
+        //Runnable Interface with anonymous class
+        Runnable threadAnonymous = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Thread 1 start");
+            }
+        };
+
+        Runnable threadLambda = () -> System.out.println("Thread 2 Start");
+        threadAnonymous.run();
+        threadLambda.run();
+    }
+}
