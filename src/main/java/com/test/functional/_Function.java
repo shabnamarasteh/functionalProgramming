004715ce0e9f5111587e202d886b7f8a736a7dc7
shabnamarasteh <64069037+shabnamarasteh@users.noreply.github.com>
2021-05-10 11:57:44 -0800

    Functional interface 'function'

---
+++ src/main/java/com/test/functional/_Function.java
@@ -0,0 +1,26 @@
+package com.test.functional;
+
+import java.util.function.Function;
+
+public class _Function {
+    public static void main(String[] args) {
+        //Function interface : first argument is input and second is output type
+        //This method applies the given function on its only argument
+
+        Function<Integer, Integer> doubleValueFunction =
+        value -> value * value;
+        System.out.println(doubleValueFunction.apply(10));
+
+        //if use andThen instead Of apply we have a new Function
+        doubleValueFunction = doubleValueFunction.andThen(value -> 2*value);
+        System.out.println(doubleValueFunction.apply(5));
+
+//         first call the Function passed as parameter to compose(),
+//         and then it will call the Function which compose() was called on.
+
+        Function<Integer, Integer> multiply = (value) -> value * 2;
+        Function<Integer, Integer> add = (value) -> value + 3;
+        Function<Integer, Integer> addThenMultiply = multiply.compose(add);
+        System.out.println("-----compose : "+addThenMultiply.apply(5));
+    }
+}
